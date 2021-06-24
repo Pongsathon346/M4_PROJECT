5fb84295162a802f2681459c87886f9243ff92e1
pongsathon346 <pongsathon_j@cmu.ac.th>
2021-06-24 14:01:01 +0700

    Add Favorite function

--- script.js
+++ script.js
@@ -2,6 +2,8 @@
 document.getElementById('searchButton').addEventListener('click',function(){
     let name = document.getElementById('inputSearch').value
     console.log(name)
+    hideAll()
+    SResults.style.display='block'
     querySearch(name)
 })
 
@@ -68,6 +70,69 @@ function onLoad(){
       querySearch()
 }
 
+function ListQuery(){
+    fetch(`https://se104-project-backend.du.r.appspot.com/movies/632110346`).then((response)=>{
+        return response.json()
+    }).then( data=>{
+        myList(data)
+    })
+}
+
+function myList(list){
+    const myListTable = document.getElementById('fav-results')
+    myListTable.innerHTML = ''
+    for(mylist of list){
+        showFav(mylist)
+    }
+}
+
+function showFav(data){ 
+    const searchResults = document.getElementById('fav-results');
+    let card = document.createElement('div')
+        let image = document.createElement('img')
+        image.src = data.image_url
+        card.appendChild(image)
+        let title = document.createElement('h1')
+        title.innerHTML = data.title
+        card.appendChild(title)
+        let detail = document.createElement('p')
+        detail.innerHTML = data.synopsis
+        card.appendChild(detail)
+        let button = document.createElement('button')
+        button.innerHTML = "add List"
+        button.addEventListener('dblclick', function(){
+            let confirmadd = confirm(`คุณอยากเพิ่ม ${data.title}  เข้าไปใน Favorite หรือไม่`)
+            if(confirmadd){
+                addfavDB(data)
+            }
+        })
+        card.appendChild(button)
+        searchResults.appendChild(card)
+        
+    }
+
+function addSearchToDB(movie){
+    var id=1
+    let body=`{"url":"${movie.url}","image_url":"${movie.image_url}","title":"${movie.title}","synopsis":"${movie.synopsis}","type":"${movie.type}","episodes":"${movie.episodes}","score":"${movie.score}","rated":"${movie.rated}","id":"${id}"}`
+    fetch(`https://se104-project-backend.du.r.appspot.com/movies `,{
+            method:'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: `{"id":"632110342","movie":${body}}`
+        }).then(response=>{
+            if(response.status == 200){
+                return response.json()
+            }else{
+                throw Error(response.statusText)
+            }
+        }).then(data=>{
+            alert('Success')
+            id++
+        }).catch(error=>{
+            alert('Error')
+        })
+}
 
 
 
@@ -95,10 +160,28 @@ function addfavDB(anime){
 }
 
 
+
+
 document.getElementById('homeMenu').addEventListener('click', (event) => {
-    
+    hideAll()
+    SResults.style.display='block'
     querySearch()
 })
+
+document.getElementById('favMenu').addEventListener('click', (event) => {
+    hideAll()
+    FResults.style.display='block'
+    ListQuery()
+})
+
+var SResults = document.getElementById('search-results')
+var FResults = document.getElementById('fav-results')
+
+function hideAll(){
+    SResults.style.display='none'
+    FResults.style.display='none'
+}
+
 
 
 
